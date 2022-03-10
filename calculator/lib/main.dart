1ce9e7595289b20fe49869739f07de094f6a7071
Essi Prykäri <essi.prykari@helsinki.fi>
2022-03-10 22:55:46 +0200

    added display & made buttons responsive to screen size

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -4,8 +4,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget { 
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,100 +17,94 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
-
   //TÄMÄ RAKENTAA UUDEN BUTTONIN NÄYTTÖÖN CONTAINER EI EHKÄ VÄLTTÄMÄTÖN?
-  Widget buildButton(String buttonText){
-    return(
-      Container(
-        margin: new EdgeInsets.all(10.0),
-        child: ElevatedButton(
-          child: Text(buttonText),
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-              fixedSize: const Size(60, 60),
-              shape: const CircleBorder(),
-              textStyle:
-                  const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-        ),
-      )
-    );
+  //CONTAINERIIN SAA MARGINIT
+  Widget buildButton(String buttonText) {
+    return Expanded(
+        child: Container(
+      margin: const EdgeInsets.all(10.0),
+      child: ElevatedButton(
+        child: Text(buttonText),
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            fixedSize: const Size(60, 60),
+            shape: const CircleBorder(),
+            textStyle:
+                const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+      ),
+    ));
   }
 
   //TÄMÄ RAKENTAA UUDEN OPERANDBUTTONIN NÄYTTÖÖN CONTAINER EI EHKÄ VÄLTTÄMÄTÖN?
-    Widget operandButton(String operand){
-    return(
-      Container(
-        margin: new EdgeInsets.all(10.0),
-        color: Colors.orange,
-        child: MaterialButton(
-          onPressed:() => {},
-          child: new Text(
-              operand,
-              style:  TextStyle(
-              fontSize: 30, fontWeight: FontWeight.bold),
-          ),
+  //KATOAAKO VÄRI JOS CONTAINERIN JÄTTÄÄ POIS?
+  Widget operandButton(String operand) {
+    return Expanded(
+        child: Container(
+      margin: const EdgeInsets.all(10.0),
+      color: Colors.orange,
+      child: MaterialButton(
+        onPressed: () => {},
+        child: Text(
+          operand,
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
-      )
-    );
+      ),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: new Container(
-
-        child: new Column(children: <Widget>[
-
-          const Text("0"),
-
-         new Row(
-          children: [
-            buildButton("7"),
-            buildButton("8"),
-            buildButton("9"),
-            operandButton("-"),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-         new Row(
-          children: [
-            buildButton("4"),
-            buildButton("5"),
-            buildButton("6"),
-            operandButton("+"),
+        body: Column(
+          children: <Widget>[
+            Container(
+                margin: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(10.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.blue)),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  '0',
+                  style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+                )),
+            Column(children: [
+              Row(children: [
+                buildButton("7"),
+                buildButton("8"),
+                buildButton("9"),
+                operandButton("-"),
+              ]),
+              Row(children: [
+                buildButton("4"),
+                buildButton("5"),
+                buildButton("6"),
+                operandButton("+"),
+              ]),
+              Row(children: [
+                buildButton("1"),
+                buildButton("2"),
+                buildButton("3"),
+                Expanded(child: Text('')), //PITÄÄ RIVIT SAMANLEVYISINÄ
+              ]),
+              Row(children: [
+                buildButton("C"),
+                buildButton("0"),
+                buildButton("."),
+                operandButton("="),
+              ])
+            ])
           ],
-        ),
-        new Row(
-          children: [
-            buildButton("1"),
-            buildButton("2"),
-            buildButton("3"),
-          ],
-        ),
-        new Row(
-          children: [
-            buildButton("C"),
-            buildButton("0"),
-            buildButton("."),
-            operandButton("="),
-          ],
-        )
-
-        ],),
-      )
-    );
+        ));
   }
 }
