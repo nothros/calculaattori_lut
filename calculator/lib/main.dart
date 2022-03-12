f40605ea10bffdcb0aaa4a738d1487c24aaae86a
Erika Horros-Wallin <erika.horros@gmail.com>
2022-03-12 14:36:13 +0200

    Added colors and improve layout

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -9,9 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Awesome calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: new ThemeData(scaffoldBackgroundColor: Colors.blueGrey[700]),
       home: const MyHomePage(title: 'Awesome calculator'),
     );
   }
@@ -20,7 +18,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-  
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -30,7 +27,6 @@ class _MyHomePageState extends State<MyHomePage> {
   //Näitä tarvii näyttö
   String output = "0";
   String _output = "";
-
 
   //Buttoneiden logiikka.
   buttonPressed(String buttonText){
@@ -54,34 +50,100 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   //Funkkarit loppuu
 
+  //Näyttö
+  Widget buildTextArea() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 22.0),
+        //        padding: const EdgeInsets.all(10.0),
+      width: screenWidth(context, dividedBy: 4)*3 -44, //tätä pitää vähän säätää. (luo siis 3/4 näyttöä leveen)
+      height: 120,//tätä pitää vähän säätää. (1/5 näyttöä korkea)
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        color: Colors.lightGreen[600],
+      ),
+      alignment: Alignment.centerRight,
+      child: Text(
+        output,
+        style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+      ));
+  }
   //Numerobuttonit
   Widget buildButton(String buttonText) {
-    return Expanded(
+    if (buttonText == "C"){
+      return Expanded(
         child: Container(
-      margin: const EdgeInsets.all(10.0),
       child: ElevatedButton(
         child: Text(buttonText),
         onPressed: () => buttonPressed(buttonText),
         style: ElevatedButton.styleFrom(
-            fixedSize: const Size(80, 80),
+          primary: Colors.orange[800],
+           minimumSize: const Size(80, 80),
             shape: const CircleBorder(),
             textStyle:
-                const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, fontFamily: "VT323",)),
+                const TextStyle(
+                  color: Colors.white, 
+                  fontSize: 30, 
+                  fontWeight: FontWeight.bold,
+                )),
       ),
     ));
+    }
+    return Expanded(
+      child: Container(
+        child: ElevatedButton(
+          child: Text(buttonText),
+            onPressed: () => buttonPressed(buttonText),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.blueGrey[900],
+              minimumSize: const Size(80, 80),
+              shape: const CircleBorder(),
+              textStyle:
+                const TextStyle(color: Color.fromARGB(255, 255, 224, 178), 
+                fontSize: 30, 
+                fontWeight: FontWeight.bold,
+              )
+            ),
+        ),
+      )
+    );
   }
 
   //Operandibuttonit vielä säätöä
   Widget operandButton(String operand) {
-    return Expanded(
+    if (operand == "="){
+      return Expanded(
         child: Container(
-      margin: const EdgeInsets.all(10.0),
-      color: Colors.orange,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.orange[700],
+          ),
+          margin: const EdgeInsets.all(10.0),
+          child: MaterialButton(
+            height: 50,
+            onPressed: () => buttonPressed(operand),
+            child: Text(
+              operand,
+              style: new TextStyle(fontSize: 30, 
+              fontWeight: FontWeight.bold, 
+              color: Colors.white),
+            ),
+          ),
+        ));
+    }
+    
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.orange[200],
+        ),
       child: MaterialButton(
+        height: 50,
         onPressed: () => buttonPressed(operand),
         child: Text(
           operand,
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          style: new TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
       ),
     ));
@@ -89,25 +151,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(
+    return Scaffold(     
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               //Numeronäyttö
-              Row(children: [Container(
-                margin: const EdgeInsets.all(10.0),
-                padding: const EdgeInsets.all(10.0),
-                width: screenWidth(context, dividedBy: 4)*3-10, //tätä pitää vähän säätää. (luo siis 3/4 näyttöä leveen)
-                height: screenHeight(context, dividedBy: 5),//tätä pitää vähän säätää. (1/5 näyttöä korkea)
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.blue)),
-                alignment: Alignment.centerRight,
-                child: Text(
-                  output,
-                  style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
-                ))]),
+              Row(children: [
+                buildTextArea(),
+              ]),
                 //Numeronäyttö loppuu
               Row(children: [
                 buildButton("7"),
@@ -125,7 +178,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 buildButton("1"),
                 buildButton("2"),
                 buildButton("3"),
-                Expanded(child: Text('')), //PITÄÄ RIVIT SAMANLEVYISINÄ
+                const Expanded(child: Text('')), //PITÄÄ RIVIT SAMANLEVYISINÄ
               ]),
               Row(children: [
                 buildButton("C"),
@@ -134,7 +187,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 operandButton("="),
               ])
             ])
-
+          )
         );
   }
 }
